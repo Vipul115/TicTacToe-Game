83fe630917541fb579cfdf71392b022588eecffa
Vipul115 <vipulrustagi11@gmail.com>
2017-10-16 22:23:02 +0530

    Completed

---
+++ src/tictactoe/TicTacToe.java
@@ -0,0 +1,22 @@
+package tictactoe;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ * This is a simple TicTacToe game.
+ */
+public class TicTacToe 
+{
+    public static void main(String[] args) 
+    {
+        JFrame ticTacToe = new Frame();
+        ticTacToe.setTitle("Vipul's TicTacToe Game!");
+        ticTacToe.setSize(500, 500);
+        ticTacToe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ticTacToe.setLocationRelativeTo(null);
+        ticTacToe.setVisible(true);
+        
+             
+    }
+} // 
